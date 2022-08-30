929b1ca6386f6e7c3957d76467b0804bdc459a52
bastienapp <bkrettly@simplon.co>
2022-08-30 10:29:25 +0200

    fix comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import TodoList from "./components/TodoList";
 
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
+
 function App() {
   const [todos, setTodos] = useState([]);
 
-  // Create a useEffect, install axios and call the API
+  // install and import axios, create an useEffect hook, and call the API
 
   return (
     <div className='App'>
