29e5283b52d7683d69a2da030195b8cb67eb7d1a
Adeline64 <adeline.lacabanne@gmail.com>
2022-09-06 14:23:07 +0200

    ajout useEffect ds import de react, ajout et installation de axios et code et appel de l'API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+// import { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 import TodoList from "./components/TodoList";
+import axios from 'axios';
 
 const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
@@ -8,6 +10,13 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   // install and import axios, create an useEffect hook, and call the API
+  useEffect(() => {
+    axios
+      .get(API_URL)
+      .then((response) => {
+        setTodos(response.data)
+      })
+  }, []);
 
   return (
     <div className='App'>
